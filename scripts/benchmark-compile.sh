da152a0f83e6ed92982c082a1da029b1a57c2562
Michal Mrena <mrena.michal@gmail.com>
2023-08-09 10:50:04 +0200

    Added benchmark script

---
+++ scripts/benchmark-compile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for TARGET in "libteddy-example-1" "libteddy-example-2"; do
+    echo -e "\e[32m${TARGET}\e[0m:"
+    hyperfine --prepare "cd build/release && make clean" \
+        "cd build/release && make ${TARGET}"
+    EXEC_SIZE=$(du -k "build/release/examples/${TARGET}" | cut -f1)
+    echo "Executable size: ${EXEC_SIZE} kiB"
+    echo ""
+done
+
+echo -e "\e[32mSource stats:\e[0m:"
+cloc ./libteddy
