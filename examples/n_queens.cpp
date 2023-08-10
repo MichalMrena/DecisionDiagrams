af9db46fd3e23129185cd15d6fe69eb9cd971d03
Michal Mrena <mrena.michal@gmail.com>
2023-08-10 13:05:52 +0200

    Added example and benchark

---
+++ examples/n_queens.cpp
@@ -0,0 +1,172 @@
+#include <libteddy/core.hpp>
+#include <libteddy/details/tools.hpp>
+#include <chrono>
+#include <iostream>
+#include <optional>
+#include <vector>
+
+/**
+ *  Implementation of the N-Queen problem adapted from Sylvan:
+ *  https://github.com/utwente-fmt/sylvan/blob/master/examples/nqueens.c
+ */
+void solve(int const n)
+{
+    teddy::bdd_manager manager(n*n, 1'000'000);
+    manager.set_cache_ratio(2);
+    manager.set_gc_ratio(0.30);
+    using bdd_t = teddy::bdd_manager::diagram_t;
+    using namespace teddy::ops;
+
+    std::vector<bdd_t> board;
+    board.reserve(static_cast<std::size_t>(n * n));
+    for (int i = 0; i < n * n; ++i)
+    {
+        board.push_back(manager.variable(i));
+    }
+
+    bdd_t result = manager.constant(1);
+
+    // rows
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            bdd_t tmp = manager.constant(1);
+            for (int k = 0; k < n; ++k){
+                if (j != k)
+                {
+                    tmp = manager.apply<AND>(
+                        tmp,
+                        manager.negate(board[static_cast<std::size_t>(i*n + k)])
+                    );
+                }
+            }
+            tmp = manager.apply<OR>(
+                tmp,
+                manager.negate(board[static_cast<std::size_t>(i*n + j)])
+            );
+            result = manager.apply<AND>(result, tmp);
+        }
+    }
+
+    // cols
+    for (int j = 0; j < n; ++j)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            bdd_t tmp = manager.constant(1);
+            for (int k = 0; k < n; ++k)
+            {
+                if (i != k)
+                {
+                    tmp = manager.apply<AND>(
+                        tmp,
+                        manager.negate(board[static_cast<std::size_t>(k*n + j)])
+                    );
+                }
+            }
+            tmp = manager.apply<OR>(
+                tmp,
+                manager.negate(board[static_cast<std::size_t>(i*n + j)])
+            );
+            result = manager.apply<AND>(result, tmp);
+        }
+    }
+
+    // rising diagonals
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            bdd_t tmp = manager.constant(1);
+            for (int k = 0; k < n; ++k)
+            {
+                if (j+k >= i && j+k < n+i && k != i)
+                {
+                    tmp = manager.apply<AND>(
+                        tmp,
+                        manager.negate(
+                            board[static_cast<std::size_t>(k*n + (j+k-i))]
+                        )
+                    );
+                }
+            }
+            tmp = manager.apply<OR>(
+                tmp,
+                manager.negate(board[static_cast<std::size_t>(i*n + j)])
+            );
+            result = manager.apply<AND>(result, tmp);
+        }
+    }
+
+    // falling diagonals
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            bdd_t tmp = manager.constant(1);
+            for (int k = 0; k < n; ++k)
+            {
+                if (j+i >= k && j+i < n+k && k != i)
+                {
+                    tmp = manager.apply<AND>(
+                        tmp,
+                        manager.negate(
+                            board[static_cast<std::size_t>(k*n + (j+i-k))]
+                        )
+                    );
+                }
+            }
+            tmp = manager.apply<OR>(
+                tmp,
+                manager.negate(board[static_cast<std::size_t>(i*n + j)])
+            );
+            result = manager.apply<AND>(result, tmp);
+        }
+    }
+
+    // place queens
+    for (int i = 0; i < n; ++i) {
+        bdd_t tmp = manager.constant(0);
+        for (int j = 0; j < n; ++j) {
+            tmp = manager.apply<OR>(
+                tmp,
+                board[static_cast<std::size_t>(i*n + j)]
+            );
+        }
+        result = manager.apply<AND>(result, tmp);
+    }
+
+    std::cout << "bdd node-count:      "
+              << manager.get_node_count(result)
+              << "\n";
+    std::cout << "number of solutions: "
+              << manager.satisfy_count(1, result)
+              << "\n";
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Please specify the number of queens.\n";
+        return 1;
+    }
+
+    const std::optional<int> nOpt = teddy::utils::parse<int>(argv[1]);
+    if (not nOpt)
+    {
+        std::cerr << "Please specify the number of queens.\n";
+        return 1;
+    }
+    const int n = *nOpt;
+
+    const auto before = std::chrono::high_resolution_clock::now();
+    solve(n);
+    const auto after = std::chrono::high_resolution_clock::now();
+    const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+        after - before
+    );
+    std::cout << "elapsed time:        " << elapsed.count() << "ms" << "\n";
+
+}
--- scripts/benchmark-compile.sh
+++ scripts/benchmark-compile.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-for TARGET in "libteddy-example-1" "libteddy-example-2"; do
-    echo -e "\e[32m${TARGET}\e[0m:"
+EXAMPLES="libteddy-example-1 libteddy-example-2"
+
+echo -e "\e[32mExamples:\e[0m"
+echo ""
+
+for TARGET in $EXAMPLES; do
+    echo -e "\e[33m${TARGET}\e[0m:"
     hyperfine --prepare "cd build/release && make clean" \
         "cd build/release && make ${TARGET}"
     EXEC_SIZE=$(du -k "build/release/examples/${TARGET}" | cut -f1)
@@ -9,5 +14,21 @@ for TARGET in "libteddy-example-1" "libteddy-example-2"; do
     echo ""
 done
 
-echo -e "\e[32mSource stats:\e[0m:"
+echo -e "\e[32mOverall:\e[0m"
 cloc ./libteddy
+
+SRC_DIR="./libteddy/"
+INCLUDE_PATTERN="^#include\s[<].*[>]$"
+TMP_FILE="__tmp.cpp"
+
+grep -r $SRC_DIR -e $INCLUDE_PATTERN | cut -d: -f2 | sort | uniq > $TMP_FILE
+echo "#include <libteddy/reliability.hpp>" >> $TMP_FILE
+echo "int main(){}" >> $TMP_FILE
+
+POST_PRE_LINE_COUNT=$(g++ -I. -E examples/example_2.cpp | wc -l)
+
+echo ""
+echo -e "\e[33mLine count post-processed:\e[0m ${POST_PRE_LINE_COUNT}"
+echo ""
+
+rm $TMP_FILE
