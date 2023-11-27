436f20cbf030313c0d2d4f418b24503b9dc463d7
Michal Mrena <mrena.michal@gmail.com>
2023-11-27 08:57:06 +0100

    Added vector to matrix wrap

--- libteddy/details/symbolic_probabilities.hpp
+++ libteddy/details/symbolic_probabilities.hpp
@@ -3,6 +3,7 @@
 
 #include <libteddy/details/config.hpp>
 #include <libteddy/details/types.hpp>
+#include <cassert>
 
 #ifdef LIBTEDDY_SYMBOLIC_RELIABILITY
 #include <ginac/ginac.h>
@@ -154,13 +155,71 @@ concept symprob_matrix = requires(Probabilities probs, int32 index, int32 value)
                           } -> std::convertible_to<expression>;
                       };
 
+namespace details
+{
+/**
+ *  \brief Helper for vector wrap
+ *
+ *  Returns expression of probability that the component is in state 1 `p_{i,1}`
+ *  from \p vec or in case we need the probability that it is in state 0
+ *  it calculates it as `1 - p_{i,1}`.
+ */
+template<class Vector>
+class vector_to_matrix_proxy
+{
+public:
+    vector_to_matrix_proxy(Vector const& vec, std::size_t const index) :
+        index_(index),
+        vec_(&vec)
+    {
+    }
+
+    auto operator[] (std::size_t const value) const
+    {
+        assert(value == 1 || value == 0);
+        expression expr = (*vec_)[index_];
+        return value == 1 ? expr : complement(expr);
+    }
+
+private:
+    std::size_t index_;
+    Vector const* vec_;
+};
+
+/**
+ *  \brief Wraps prob. vector so that it can be used as matrix
+ *
+ *  Algorithms working with probability require matrix \c ps such that
+ *  \c ps[i][s] expression of probability that component \c i is in state \c s
+ *  Since for BSS we only need to know probabilities that the component
+ *  is in state 1 (p1) we can "fake" the matrix by wrapping the vector and
+ *  calculating p0 as 1-p1
+ */
+template<class Vector>
+class vector_to_matrix_wrap
+{
+public:
+    vector_to_matrix_wrap(Vector const& vec) : vec_(&vec)
+    {
+    }
+
+    auto operator[] (std::size_t const index) const
+    {
+        return vector_to_matrix_proxy<Vector>(*vec_, index);
+    }
+
+private:
+    Vector const* vec_;
+};
+} // namespace details
+
 /**
  *  \brief Wraps \p distVector so that it can be viewed as n x 2 matrix
  */
 template<symprob_vector Ps>
 auto as_matrix (Ps const& distVector)
 {
-    // TODO
+    return details::vector_to_matrix_wrap<Ps>(distVector);
 }
 
 /**
