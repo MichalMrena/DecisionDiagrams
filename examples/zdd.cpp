0e10617ed550767bbd98cee34747e2983eb2f37d
Rizoto1 <adrianko952@gmail.com>
2025-11-06 10:44:47 +0100

    zdd init commit

---
+++ examples/zdd.cpp
@@ -0,0 +1,8 @@
+#include <libteddy/impl/zdd_manager.hpp>
+
+
+int main()
+{
+    teddy::zdd_manager manager;
+    return 0;
+}
---
+++ libteddy/impl/zdd_manager.hpp
@@ -0,0 +1,14 @@
+#ifndef LIBTEDDY_DETAILS_ZDD_MANAGER_HPP
+#define LIBTEDDY_DETAILS_ZDD_MANAGER_HPP
+
+namespace teddy
+{
+
+class zdd_manager
+{
+
+};
+
+}
+
+#endif
